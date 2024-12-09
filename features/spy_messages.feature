5cba615bfc1b1b79e56025f8a0e4b367f4755b57
Tyler Rogel <d00476330@utahtech.edu>
2024-12-09 13:32:50 -0700

    please work workflow

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -6,7 +6,17 @@ So that I can chat with my spy friends like a pro.
 Scenario: I can successfully encode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
     # write your steps here
-
+    When I set "test encode" to the inputfield "#letters"
+    When I select the option with the value "1" for element "#shift-amount"
+    When I click on the element "#submit"
+    When I pause for 200ms
+    Then I expect that element "#decoded_message" contains the text "uftu fodpef"
 Scenario: I can successfully decode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
     # write your steps here
+    When I set "uftu efdpef" to the inputfield "#letters"
+    When I select the option with the text "Decode" for element "#decoder-setting"
+    When I select the option with the value "1" for element "#shift-amount"
+    When I click on the element "#submit"
+    When I pause for 200ms
+    Then I expect that element "#decoded_message" contains the text "test decode"
--- test_brute.py
+++ test_brute.py
@@ -1,6 +1,6 @@
-import pytest
+import pytest #type: ignore
 from brute import Brute
-
+from unittest.mock import Mock
 todo = pytest.mark.skip(reason='todo: pending spec')
 
 def describe_Brute():
@@ -11,8 +11,38 @@ def describe_Brute():
 
     def describe_bruteOnce():
         # write your test cases here
-        pass
+        def it_returns_true_on_match():
+            cracker = Brute("TEST")
+            assert cracker.bruteOnce("TEST")
+
+        def it_returns_false_on_mismatch():
+            cracker = Brute("TEST")
+            assert not cracker.bruteOnce("WRONG")
+        
+        def it_returns_false_on_empty_string():
+            cracker = Brute("TEST")
+            assert not cracker.bruteOnce("")
+
 
     def describe_bruteMany():
         # write your test cases here
-        pass
+        def it_returns_positive_time_on_match():
+            cracker = Brute("TEST")
+            mock_guess = Mock()
+            mock_guess.return_value = "TEST"
+            cracker.randomGuess = mock_guess
+            assert cracker.bruteMany() > 0
+        
+        def it_returns_negative_one_on_mismatch():
+            cracker = Brute("TEST")
+            mock_guess = Mock()
+            mock_guess.return_value = "WRONG"
+            cracker.randomGuess = mock_guess
+            assert cracker.bruteMany(100) == -1
+        
+        def it_returns_negative_one_on_empty_string():
+            cracker = Brute("TEST")
+            mock_guess = Mock()
+            mock_guess.return_value = ""
+            cracker.randomGuess = mock_guess
+            assert cracker.bruteMany(100) == -1
